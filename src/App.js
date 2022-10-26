59e5e19f93bdec01b31cd60ff144c00b2ede6662
siddharthbeniwal023 <siddharthbeniwal2001@gmail.com>
2022-10-26 14:51:26 +0530

    Added CSS Files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from "./Header"
+import "./App.css"
+
 function App() {
   return (
     <div className="App">
-      <h1>Broken Ties</h1>
+          <Header/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css"
+const Header = function(){
+    
+    return (
+        <div className="body">
+            <a href="#">Songs</a>
+            
+            <div className="heading">
+        Broken Ties
+        </div>
+        <a href="#">Spotify</a>
+            </div>
+    )
+}
+export default Header;
