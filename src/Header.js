75cfca9f042886e565f8a31254fdfc495809a14f
siddharthbeniwal023 <siddharthbeniwal2001@gmail.com>
2022-10-27 12:45:38 +0530

    Centered the Nav Bar

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./Header.css"
-const Header = function(){
-    
+const Header = function () {
+
     return (
-        <div className="body">
-            <a href="#">Songs</a>
-            
-            <div className="heading">
-        Broken Ties
-        </div>
-        <a href="#" id="spotify">Spotify</a>
-        
+        <div>
+            <div className="body">
+                <a href="#">Youtube</a>
+                <div className="heading">
+                    Broken Ties
+                </div>
+                <a href="#" id="spotify">Spotify</a>
             </div>
+        </div>
     )
 }
 export default Header;
