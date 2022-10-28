ace1f8d232aa57bf3b1020f1a365d76e91fce2da
siddharthbeniwal023 <siddharthbeniwal2001@gmail.com>
2022-10-28 11:57:44 +0530

    Applied animation to heading (bt)

--- src/Header.js
+++ src/Header.js
@@ -5,11 +5,17 @@ const Header = function () {
     return (
         <div>
             <div className="body">
-            <a href="#">Home</a>
-            <a href="#">About</a>
+                <a href="#">Home</a>
+                <a href="#">About</a>
                 <a href="#">Music</a>
                 <div className="heading">
-                    Broken Ties
+                    <button className="headingbtn">
+                        <span>B</span><span>T</span>
+                        </button>
+                    {/* <div className="dropdown">
+                        <a href="#">Let's get on a ride
+                        </a>
+                    </div> */}
                 </div>
                 <a href="#" id="spotify">Merch</a>
                 <a href="#">Tour</a>
