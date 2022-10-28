892db8afc6cffe079f6403c5ef8e21ae602e6eab
siddharthbeniwal023 <siddharthbeniwal2001@gmail.com>
2022-10-28 12:46:56 +0530

    Applied transitions to the heading

--- src/Header.js
+++ src/Header.js
@@ -5,21 +5,21 @@ const Header = function () {
     return (
         <div>
             <div className="body">
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Music</a>
+                <a href="#" id="ln">Home</a>
+                <a href="#" id="ln">About</a>
+                <a href="#" id="ln">Music</a>
                 <div className="heading">
                     <button className="headingbtn">
                         <span>B</span><span>T</span>
-                        </button>
+                    </button>
                     {/* <div className="dropdown">
                         <a href="#">Let's get on a ride
                         </a>
                     </div> */}
                 </div>
-                <a href="#" id="spotify">Merch</a>
-                <a href="#">Tour</a>
-                <a href="#">Contact </a>
+                <a href="#" id="ln">Merch</a>
+                <a href="#" id="ln">Tour</a>
+                <a href="#" id="ln">Contact </a>
             </div>
         </div>
     )
